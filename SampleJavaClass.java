c24b31c4d7b8d89a35771db3848dbae723b668d6
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 <harshravi324@gmail.com>
2022-01-28 21:05:12 +0530

    Some sample java file added

---
+++ SampleJavaClass.java
@@ -0,0 +1,18 @@
+public class SampleJavaClass 
+{
+    // fields or members
+    // int, double, class variable
+    private int gear;
+    private int rpm;
+
+    // methods 
+    // Functionality to be used based on the instance of the class
+
+    public void accelerate(){}
+    public void braking(){}
+
+    public static void main()
+    {
+
+    }
+}
