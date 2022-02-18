16e149f6305b363bce8df3d2965ae7a048548065
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 <harshravi324@gmail.com>
2022-02-18 21:20:37 +0530

    MyPolyphormism file added

---
+++ MyPolymorphism.java
@@ -0,0 +1,36 @@
+
+
+// same function, different behavior
+// same function name, different implementation/definition
+
+// Static Polymorphism
+
+// Dynamic Polymorphism
+
+public class MyPolymorphism {
+
+    private DemoComposition _dc; // example of composition, which is also reuse of code.
+
+}
+
+abstract class DemoAbstract
+{
+    public abstract void someMethodToBeimplemented();
+    public void reuseMethod()
+    {
+        //
+    }
+}
+
+
+class DemoComposition
+{
+
+}
+
+class Main{
+    public static void main(String[] args)
+    {
+
+    }
+}
