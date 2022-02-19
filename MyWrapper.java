f943fabf51183ad79869748db6b3493b11a764c8
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 <harshravi324@gmail.com>
2022-02-19 22:09:19 +0530

    Wrapper file added

---
+++ MyWrapper.java
@@ -0,0 +1,17 @@
+// log4j - external library
+
+public class MyWrapper {
+    private static log4j _logginlib = new log4j();
+    public logtoFile(String filename)
+    {
+
+    }
+}
+
+class Main
+{
+    public static void main(String[] args)
+    {
+
+    }
+}
