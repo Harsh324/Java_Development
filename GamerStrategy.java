13d16ef3c1d64914ddc9c52bb7e8cf0af36c1a5b
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 <harshravi324@gmail.com>
2022-04-18 19:44:43 +0530

    Gamer Strategy file addded

---
+++ GamerStrategy.java
@@ -0,0 +1,21 @@
+//Characters
+//SumoJack - sumo wrestler - Roll, Punch
+//BruceLee - Karate expert - Roll, Punch, Kick
+//SuperHanuman - Indian super hero - Roll, Punch, Kick, Jump
+//ChunLi - Taekwondo - Roll, Punch, Jump
+//BadaBheem - street fighter - Roll, Kick
+
+//Behaviors
+//Jumping - HighJump, LongJump, SuperJump
+//Kick - JumpKick, LightningKick, PowerKick
+//Punch - PowerPunch, IronPunch
+//Roll - varies by speed
+
+
+
+public class GamerStrategy {
+    public static void main(String[] args)
+    {
+
+    }
+}
